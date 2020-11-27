ac05c680408bbf5749b88b063945dd16fe0ab0b8
ADMIN <sridharkalyan26@gmail.com>
2020-11-27 22:08:27 +0530

    commmit

---
+++ src/test/java/com/kal/smoke/HomePageTest2.java
@@ -0,0 +1,15 @@
+package com.kal.smoke;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest2 {
+	
+	
+	@Test
+	public void Test12() {
+		
+		System.out.println("Testing2 is in progress...");
+		
+	}
+
+}
